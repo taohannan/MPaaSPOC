ab60dc714735e68b2dc8530456fdd4c4073ca1d4
Hannan Isa <ahannan@bankislam.com.my>
2025-03-12 12:36:57 +0800

    MPaaS maven configuration added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,6 +24,13 @@ dependencyResolutionManagement {
             url = uri("https://mvn.cloud.alipay.com/nexus/content/repositories/open/")
             name = "alipay"
         }
+        maven {
+            url = uri("https://packages.aliyun.com/63dc774a9dee9309492b993a/maven/repo-jpkdy/")
+            credentials {
+                username = "626668353c6b5273a0ce984d"
+                password = "t[4AU)JOBzXN"
+            }
+        }
     }
 }
 
